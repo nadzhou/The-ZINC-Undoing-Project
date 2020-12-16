e254c442a1e72387a525191500b74a2aa4fb0e91
nadzhou <ahmaddnavid@gmail.com>
2020-12-17 00:12:55 +0500

    Made the whole code functional

--- scripts/sdf2pdb.py
+++ scripts/sdf2pdb.py
@@ -5,14 +5,13 @@ from __future__ import print_function
 import time
 import argparse as ap
 from rdkit import Chem
+
 from rdkit.Chem import AllChem
 from pathlib import Path
-
 start_time = time.time()
 
 
 def main(): 
-
     args = args_sdf2pdb()
     drug_folder = retrieve_drug_folder(args)
     sdf2pdb(drug_folder, args)
@@ -20,23 +19,21 @@ def main():
 
 
 def args_sdf2pdb(): 
-    # Turning the input and parsing via argparse.
+    """Get the drug folder and number of conformations from user in command line"""
     parser = ap.ArgumentParser(description=__doc__)
     parser.add_argument("drug_input", help='Input file in SDF format')
-    # Optional argument that will generate ay number of 
-    # conformations user inputs.
     parser.add_argument("--c", type=int, default=1,
                 help="get number of  conformations needed to be generated")
-    args = parser.parse_args()
+
+    return parser.parse_args()
 
 
-def retrieve_drug_folder(args): 
-    # Sdf is converted to Smiles. Space
+def retrieve_drug_folder(args: str): 
     return Path(args.drug_input)
 
 
-def sdf2pdb(drug_folder):  
-    # This will only retrieve the sdf files in a folder. 
+def sdf2pdb(drug_folder: str):  
+    """Convert the whole drug directory into PDB structures."""
     for drug in drug_folder.rglob("*.sdf"):
 
         print('Initializing conversion')
